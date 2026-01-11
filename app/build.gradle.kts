0411cc90397d9516008607f77886273007bf8a3d
pete <pete@hurth.net>
2026-01-11 16:29:36 -0600

    Bump version to 2.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.blemqttbridge"
         minSdk = 26  // Android 8.0
         targetSdk = 34
-        versionCode = 32
-        versionName = "2.5.2"
+        versionCode = 33
+        versionName = "2.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
